f493b96883646525c35298b9c0ec88828e3a2031
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2021-05-23 01:26:46 +0530

    server last commit

--- back-end/server.js
+++ back-end/server.js
@@ -6,14 +6,16 @@ const BuyerRoutes = require('../back-end/routes/buyer.router');
 
 require('./dal/connection');
 
-const app = new Express();
-app.use(cors());
-app.use(Express.json());
+const app = new Express();//creating the express server
 
+app.use(cors());//enabling cors policy for transaction between two different ports
+app.use(Express.json());//accepting the json data from the request
+
+//mapping the http requests to respective service routes
 app.use("/seller",SellerRoutes);
 app.use("/buyer",BuyerRoutes);
 
-
+//assigning the server to a port
 app.listen(5000, err => {
     if (err) {
         console.log(err);
